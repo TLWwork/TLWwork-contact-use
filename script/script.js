3a731b94ca471bc19f77a664b86caefe908e50e9
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2023-03-11 21:54:21 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,34 @@
 "use strict"
 console.log("salut sunt eu daniel luntras ")
 
+
+
+const active3 = document.getElementById('opacity1')
+const active4 = document.getElementById('opacity1') 
+
+
+
+function phone(){
+    const active1 = document.getElementById('opacity1')   
+    if(active1.style.display === "block"){
+        active1.style.display = "none";
+    }
+    else{
+        active1.style.display = "block";
+    }
+};
+
+function email(){
+    const active2 = document.getElementById('opacity2')
+
+    if(active2.style.display === "none"){
+        active2.style.display = "block"
+    }else {
+        active2.style.display = "none"
+    }
+
+}
+
+
+
+
